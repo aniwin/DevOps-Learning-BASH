f5aab732f3751b7a04098657801202060682739f
Anisah Mohamud <a.mohamud97@gmail.com>
2025-06-22 22:06:26 +0100

    feat: scripts for level 1 and level 2

---
+++ bash_battle_arena/level_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+: 'Mission: Create a directory named Arena and then inside it, create three files:
+ warrior.txt, mage.txt, and archer.txt. 
+ List the contents of the Arena directory.
+ '
+
+mkdir Arena && cd $_
+touch warrior.txt mage.txt archer.txt
+ls
+ 
